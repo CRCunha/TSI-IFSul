21718f3870ff01cd3d13b5ed349b7150833b548c
Cristian Raffi Cunha <cristianraffi@bol.com.br>
2019-04-24 09:31:03 -0300

    Trabalho PHP

---
+++ Trabalho PHP/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cerâmica Ypê</title>
+    <!--METAS-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="portfolio, web, developer, design, layout, UI, UX">
+    <meta name="language" content="PT-BR">
+    <meta name="author" content="Nome">
+    <meta name="robots" ontent="noindex">
+    <meta name="googlebot" content="noindex">
+    <!--LINKS-->
+    <link rel="stylesheet" href="CSS/GERAL/GERAL.css" ">
+    <link rel="stylesheet " href="CSS/NORMALIZE/NORMALIZE.css ">
+    <link rel="stylesheet " href="CSS/HEADER/header.css ">
+    <link rel="stylesheet " href="CSS/HEADER/nav.css ">
+    <link rel="stylesheet " href="CSS/SERVICES/services.css ">
+    <link rel="stylesheet " href="CSS/SERVICES/cards.css ">
+    <link rel="stylesheet " href="CSS/TRABALHOS/trabalhos.css ">
+    <link rel="stylesheet " href="CSS/SECTION1/section1.css ">
+    <link rel="stylesheet " href="CSS/SECTION2/section2.css ">
+    <!--FONTES-->
+    <link href="https://fonts.googleapis.com/css?family=Staatliches " rel="stylesheet ">
+    <link href="https://fonts.googleapis.com/css?family=Roboto " rel="stylesheet ">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro " rel="stylesheet ">
+    <!--FAV ICON-->
+    <link rel="shortcut icon " href="IMG/HEADER/Logo.png " />
+</head>
+<?php ?>
+<body>
+    <!-- HEADER -->
+    <div class="header ">
+        <nav>
+            <div class="lContent ">
+                <div class="logo ">
+                    <img src="IMG/HEADER/Logo.png ">
+                </div>
+            </div>
+            <div class="rContent "></div>
+        </nav>
+        <div class="headerContent ">
+            <div class="container ">
+                <div class="title ">Cerâmica Ypê</div>
+                <div class="sub ">Você é a fundação do seu lar, nós somos as paredes</div>
+                <div class="btnContainer ">
+                    <div class="btn ">
+                        <a href="admin/index.php ">DashBoard</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- SERVICES -->
+    <div class="services ">
+        <div class="title ">
+            <b>Destaques</b>
+            <div class="linha ">
+                <div class="linhaColor "></div>
+            </div>
+        </div>
+        <!--CARDS-->
+        <div class="cards ">
+            <div class="container ">
+                <!-- CARD 1-->
+                <div class="card " id="card1 ">
+                    <div class="cardHeader ">
+                        <div class="container ">
+                            <img src="IMG/SERVICES/CARDS/pictures.png ">
+                            <div class="title " style="margin-left:-32% "> Entrega</div>
+                        </div>
+                    </div>
+                    <div class="text ">
+                        <div class="container ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nulla, quod sit.</div>
+                    </div>
+                </div>
+                <!-- CARD 2-->
+                <div class="card " id="card2 ">
+                    <div class="cardHeader ">
+                        <div class="container ">
+                            <img src="IMG/SERVICES/CARDS/pictures.png ">
+                            <div class="title "> Compromisso</div>
+                        </div>
+                    </div>
+                    <div class="text ">
+                        <div class="container ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nulla, quod sit.</div>
+                    </div>
+                </div>
+                <!-- CARD 3-->
+                <div class="card " id="card3 ">
+                    <div class="cardHeader ">
+                        <div class="container ">
+                            <img src="IMG/SERVICES/CARDS/pictures.png ">
+                            <div class="title "  style="margin-left:-32% "> Serviço</div>
+                        </div>
+                    </div>
+                    <div class="text ">
+                        <div class="container ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nulla, quod sit.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--TRABALHOS-->
+    <div class="trabalhos ">
+        <div class="title ">
+            <b>Nosso Trabalho</b>
+            <div class="linha ">
+                <div class="linhaColor "></div>
+            </div>
+        </div>
+        <div class="sub ">A Cerâmica Ypê possui a qualidade e o preço que você para a sua obra.</div>
+        <div class="galeria ">
+            <div class="left ">
+                <div class="squad " style="background-image: url( 'IMG/TRABALHOS/LEFT/squad1.jpg') "></div>
+                <div class="squad " style="background-image: url( 'IMG/TRABALHOS/LEFT/squad2.jpg') " ></div>
+                <div class="squad " style="background-image: url( 'IMG/TRABALHOS/LEFT/squad3.jpg') "></div>
+                <div class="squad " style="background-image: url( 'IMG/TRABALHOS/LEFT/squad4.jpg') "></div>
+            </div>
+            <div class="right "></div>
+        </div>
+    </div>
+    <!--SECTION 1-->
+    <div class="section1 ">
+        <div class="container ">
+            <div class="image "></div>
+            <div class="text ">
+                <div class="title ">
+                    <p>Titulo relacionado a imagem</p>
+                    <div class="linha ">
+                        <div class="linhaColor "></div>
+                    </div>
+                </div>
+                <div class="content ">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus, excepturi doloribus soluta ducimus, beatae sed explicabo deleniti earum deserunt dolore rerum laboriosam. Quas repudiandae iste fugiat tenetur illo magni.</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magnam ducimus officia excepturi neque veniam illo reprehenderit, natus, quidem laborum, aliquid dicta deserunt tempore corporis! Inventore suscipit dolorem temporibus eius?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--SECTION 2-->
+    <div class="section2 ">
+        <div class="container ">
+            <div class="image "></div>
+            <div class="text ">
+                <div class="title ">
+                    <p>Titulo relacionado a imagem</p>
+                    <div class="linha ">
+                        <div class="linhaColor "></div>
+                    </div>
+                </div>
+                <div class="content ">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusamus, excepturi doloribus soluta ducimus, beatae sed explicabo deleniti earum deserunt dolore rerum laboriosam. Quas repudiandae iste fugiat tenetur illo magni.</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magnam ducimus officia excepturi neque veniam illo reprehenderit, natus, quidem laborum, aliquid dicta deserunt tempore corporis! Inventore suscipit dolorem temporibus eius?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
